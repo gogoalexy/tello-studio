fefd51a0428cba9510823da1aab075db3254fd4c
AnejL <anej.lekse@gmail.com>
2020-01-02 23:47:21 +0100

    added ui and backend for creating / opening projects

--- tello_control_ui.py
+++ tello_control_ui.py
@@ -17,13 +17,12 @@ class TelloUI:
 
     def __init__(self, tello):
         self.program_location = os.path.dirname(os.path.abspath(__file__))
-
         self.script_lock = threading.Lock()
-
+        # read and apply configs
+        self.initialiseConfigs()
         # set default workspace location
-        # TODO: read location from tello-studio metadata and create system specific paths
-        self.workspace = str(Path.home()) + "/tello-studio/"
-
+        # TODO: workspace var should be replaced with DEFAULT_PROJECT_LOCATION
+        self.workspace = self.DEFAULT_PROJECT_LOCATION
         self.tello = tello
         # initialize the root window and image panel
         self.root = Tk()
@@ -31,9 +30,44 @@ class TelloUI:
         self.root.withdraw()
         # open dialog for workspace selection
         self.workspaceSelection()
-
         self.mainUI()
 
+    # read config files on startup and set attributes accordingly
+    def initialiseConfigs(self):
+        self.configdir = Path(str(Path.home()) + "/.local/share/tello-studio")
+        self.settings_file = str(self.configdir) + "/settings"
+        self.last_project_file = str(self.configdir) + "/lastproject"
+
+        # if config dir doesn't exist initalise everything / if it does just read from it
+        if not self.configdir.exists():
+            os.mkdir(self.configdir)
+            settings_file = open(self.settings_file, "w+")
+            project_file = open(self.last_project_file, "w+")
+            settings_file.write(str(Path.home()) + "/tello-studio")
+            project_file.close()
+            settings_file.close()
+        else:
+            settings_file = open(self.settings_file, "r")
+            settings_file.close()
+
+        self.DEFAULT_PROJECT_LOCATION = open(self.settings_file, "r").read()
+        self.LAST_OPENED_PROJECT = open(self.last_project_file, "r").read()
+
+    # update lastproject config file
+    def updateLastProjectFile(self, last_project_location):
+        project_file = open(self.last_project_file, "w")
+        project_file.write(str(last_project_location))
+        project_file.close()
+
+        self.LAST_OPENED_PROJECT = str(last_project_location)
+
+    # update settings config file
+    def updateSettingsFile(self, default_project_location):
+        project_file = open(self.settings_file, "w")
+        project_file.write(str(default_project_location))
+        project_file.close()
+
+        self.DEFAULT_PROJECT_LOCATION = str(default_project_location)
 
     def workspaceSelection(self):
         top = Toplevel()
@@ -41,41 +75,96 @@ class TelloUI:
         top.geometry('500x300')
 
         msg = Message(top, text="Select an existing workspace directory or create a new one.", width=500)
-        msg.pack()
-        
+        msg.pack(side=TOP)
+
         frame = Frame(top)
         frame.pack(fill="x", padx=10)
 
         tb_path = Entry(frame)
-        tb_path.insert(0, self.workspace)
+        tb_path.insert(0, self.workspace + "/new")
         tb_path.pack(side=LEFT, fill="x", expand="yes")
-        
+
         def update():
-            path = self.setWorkspaceDialog()
+            path = self.setWorkspaceDialog("Choose workspace directory")
             # always make sure / is the last character of a path
             if path:
                 if path[:-1] != '/':
                     path += '/'
                 self.workspace = path
             tb_path.delete(0, END)
-            tb_path.insert(0, self.workspace)
+            tb_path.insert(0, self.workspace + "/new")
 
         btn_select = Button(frame, text="...", command=lambda: update())
         btn_select.pack(side=RIGHT)
 
-        def close():
-            sys.path.append(self.workspace)
+        second_frame = Frame(top)
+        second_frame.pack(fill="x", padx=10)
 
+        default_project_msg = Message(second_frame, text="Default project location is set to " + self.DEFAULT_PROJECT_LOCATION, width=500)
+        default_project_msg.pack(side=LEFT)
+
+        # update default project location
+        def updateDefault():
+            path = self.setWorkspaceDialog("Choose default project folder")
+            if not path == '':
+                self.updateSettingsFile(path)
+
+                default_project_msg.configure(text="Default project location is set to " + self.DEFAULT_PROJECT_LOCATION)
+                self.workspace = self.DEFAULT_PROJECT_LOCATION
+                tb_path.delete(0, END)
+                tb_path.insert(0, self.workspace + "/new")
+
+        btn_change_default = Button(second_frame, text="Change", command=lambda: updateDefault())
+        btn_change_default.pack(side=RIGHT)
+
+        third_frame = Frame(top)
+        third_frame.pack(fill="x", padx=10)
+
+        open_recent_msg = Message(third_frame, text="Open the most recent project", width=500)
+        open_recent_msg.pack(side=LEFT)
+
+        # get name of the last opened/created project
+        def getLastProjectName():
+            if self.LAST_OPENED_PROJECT == "":
+                return "No known projects yet"
+            else:
+                if Path(self.LAST_OPENED_PROJECT + "/.tsp").exists():
+                    last_project_file = open(self.LAST_OPENED_PROJECT + "/.tsp", "r")
+                    return last_project_file.read()
+                else:
+                    self.updateLastProjectFile("")
+                    return "No known projects yet"
+
+        last_project_label = Label(third_frame)
+        last_project_label.configure(text=getLastProjectName())
+        #last_project_label.bind("<Button-1>", command=lambda: print("yes"))
+        last_project_label.pack(side=LEFT)
+
+        # TODO: button to open last project
+        #   TODO: list multiple projects
+
+        def createOrOpenProjectDir(location, name):
+            if not Path(location).is_dir():
+                os.mkdir(location)
+                project_file = open(location + "/.tsp", "w+")
+                project_file.write(name)
+                project_file.close()
+
+            self.updateLastProjectFile(location)
+
+        def close():
+            # TODO: make this more elegant... Prompt for project name / set later?
+            createOrOpenProjectDir(tb_path.get(), tb_path.get().split("/")[-1])
+            sys.path.append(self.workspace)
             top.destroy()
             self.root.deiconify()
 
         btn_ok = Button(top, text="OK", command=lambda: close())
         btn_ok.pack(side=BOTTOM)
-    
-         
-    def setWorkspaceDialog(self):
-        return filedialog.askdirectory(title="Choose workspace directory",
-                                                 initialdir=str(self.workspace))
+
+    def setWorkspaceDialog(self, title_str):
+        return filedialog.askdirectory(title=title_str, initialdir=str(self.workspace))
+
 
     def mainUI(self):
         # set window to full size of screen
